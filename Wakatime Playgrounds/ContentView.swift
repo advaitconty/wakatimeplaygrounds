872a597dc7af66ac5c6558e55ce6b77963138105
contyadvait <advaitthepro@outlook.com>
2025-08-17 00:49:49 +0800

    Initial Commit

---
+++ Wakatime Playgrounds/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wakatime Playgrounds
+//
+//  Created by Milind Contractor on 17/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wakatime Playgrounds/Wakatime_PlaygroundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wakatime_PlaygroundsApp.swift
+//  Wakatime Playgrounds
+//
+//  Created by Milind Contractor on 17/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Wakatime_PlaygroundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
